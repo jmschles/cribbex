1534f87df40c427282777a7dd3b879a32aa547b6
Jamie Schlessinger <james.schlessinger@gmail.com>
2020-09-06 15:26:44 -0700

    Name length validation

--- lib/cribbex/validators/name_validator.ex
+++ lib/cribbex/validators/name_validator.ex
@@ -1,10 +1,13 @@
 defmodule Cribbex.NameValidator do
   def validate(name) do
     %{}
+    |> check_length(name)
     |> check_chars(name)
     |> check_uniqueness(name)
     |> summarize()
   end
+
+  defp check_chars(%{error: _} = result, _name), do: result
 
   defp check_chars(result, name) do
     if all_chars_valid?(name) do
@@ -13,6 +16,19 @@ defmodule Cribbex.NameValidator do
       Map.put(result, :error, "Names may only contain letters, numbers, underscores, and dashes")
     end
   end
+
+  defp check_length(%{error: _} = result, _name), do: result
+
+  defp check_length(result, name) do
+    case String.length(name) do
+      n when n < 3 or n > 16 ->
+        Map.put(result, :error, "Names must be between 3 and 16 characters")
+      _n ->
+        result
+    end
+  end
+
+  defp check_uniqueness(%{error: _} = result, _name), do: result
 
   defp check_uniqueness(result, name) do
     if name_taken?(name) do
